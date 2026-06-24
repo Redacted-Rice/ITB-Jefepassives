f7338e596d0b7d3704b29b3071ee598fc58989b6
TheBoardsCousin <mantiscipherphone@gmail.com>
2026-06-24 11:31:44 -0400

    Update images.lua

--- Jefepassives/scripts/images.lua
+++ Jefepassives/scripts/images.lua
@@ -8,3 +8,7 @@ modApi:appendAsset("img/weapons/passives/passive_random_swap.png",          reso
 modApi:appendAsset("img/weapons/passives/passive_cancel_nearby_attack.png", resourcePath.."img/weapons/passives/passive_cancel_nearby_attack.png")
 modApi:appendAsset("img/weapons/passives/passive_deploy_items.png",         resourcePath.."img/weapons/passives/passive_deploy_items.png")
 modApi:appendAsset("img/weapons/passives/passive_acid_rain.png",            resourcePath.."img/weapons/passives/passive_acid_rain.png")
+modApi:appendAsset("img/weapons/passives/passive_rebound.png",            resourcePath.."img/weapons/passives/passive_rebound.png")
+modApi:appendAsset("img/weapons/weapons/weapon_rebound.png",            resourcePath.."img/weapons/weapons/weapon_rebound.png")
+modApi:appendAsset("img/weapons/passives/passive_spikycleats.png", resourcePath.."img/weapons/passives/passive_spikycleats.png")
+modApi:appendAsset("img/units/passive/pawn_rebound.png", resourcePath .."img/units/passive/pawn_rebound.png")
